d47d3db930f2701f69a9461833fd5c63e5fc1fdf
Alex22071995 <al.lucas.family@gmail.com>
2023-02-07 14:21:47 +0100

    cours

--- projet web/src/app/__init__.py
+++ projet web/src/app/__init__.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'It works !'
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)   
 
 
 if __name__ == '-_main_-':
