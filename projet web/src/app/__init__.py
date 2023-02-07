0899c2a4757daa75c1d802487246f0237c5cd95c
Alex22071995 <al.lucas.family@gmail.com>
2023-02-07 15:09:06 +0100

    changement et fin de cours

--- projet web/src/app/__init__.py
+++ projet web/src/app/__init__.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+#db.init_app(app)
 
+class data(db.model):
+    __tablename__="data"
+
+    id=db.Column(db.Integer, primary_key=True)
+    rna_id=db.Column(db.String(40), nullable=True)
+    rna_id_ex=db.Column(db.String(40), nullable=True)
+    gestion=db.Column(db.String(40), nullable=True)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/liste')
+def liste():
+    return render_template('liste.html')
 
 @app.route('/about')
 def about():
